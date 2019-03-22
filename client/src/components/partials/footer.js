54e8c41f639c294ddae50a09abc504a1d41c9eb8
binay1999 <binay.dbr@gmail.com>
2019-03-23 01:03:32 +0530

    footer.js and partials.js modified

--- client/src/components/partials/footer.js
+++ client/src/components/partials/footer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "./partials.css";
 
 const Footer = props => {
   return (
     <footer>
-      <div className="text-center">FOOTER</div>
+      <div className="text-center"> &copy;2019 Tezpur University, Tezpur. &nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/ieee.tezu?ref=br_tf" rel="nofollow" title="Follow us on FaceBook"><img src="images/fb.png" width="25" alt="FBpage"  /></a></div>
+     
+          
+
     </footer>
   );
 };
