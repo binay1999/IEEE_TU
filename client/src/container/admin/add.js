2a987db4a7e3544a65dbdc798dcc13a8190d013c
Rajdeep Ghosh <rajghosh2507@gmail.com>
2019-03-25 23:26:59 +0530

    admin login page

--- client/src/container/admin/add.js
+++ client/src/container/admin/add.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { addEvent, clearNewEvent } from "../../actions";
 
+
 class AddEvent extends Component {
   state = {
     formdata: {
